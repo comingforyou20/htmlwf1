0202c89a37c55ea1d9fbec0649ee8d994aadf052
comingforyou20 <57737427+comingforyou20@users.noreply.github.com>
2020-11-30 09:50:33 +0700

    Revert "111"
    This reverts commit 7827af65fa7e4eac97cd1e9ec9fe08207d7ed9a8.

--- qr.js
+++
@@ -1,7 +0,0 @@
-
-var myvar = '<center>'+
-'<img class="mw" src="images/qr.jpg">'+
-'<h2>你给我的打电话 / 在我的微信或电报发个留言吧： <b style="color: #5480da">uyenan333</b>  我会告诉你怎么做三退</h2>'+
-'</center>';
-	
-document.write(myvar);
