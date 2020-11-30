f6477e775361bea6379083162f52c0b865e4afae
comingforyou20 <57737427+comingforyou20@users.noreply.github.com>
2020-11-30 10:41:07 +0700

    11

---
+++ menu2.js
@@ -0,0 +1,6 @@
+document.write('<table cellspacing=0 cellpadding=0 width=100% bgcolor=white style="position:fixed;top:0;z-index:1000;table-layout:fixed"><tr>\
+<td class=menu><a href="index1.html#d1" class=w>首 页</a></td><td width=1></td>\
+<td class=menu><a href="index.html#d2" class=w>下 载</a></td><td width=1></td>\
+<td class=menu><a href="index.html#d3" class=w>图 片</a></td><td width=1></td>\
+<td class=menu><a href="index.html#d4" class=w>视 频</a></td><td width=1></td>\
+<td class=menu><a href="index.html#d5" class=w>三 退</a></td></tr></table><div style="height:2em"></div>');
